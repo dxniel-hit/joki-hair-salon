4e68a019e6ffc5f0cf01242029bd6d8eaeca78e8
dxniel-hit <dancorrea698@gmail.com>
2024-10-08 23:31:34 -0500

    First commit

---
+++ src/main/java/co/edu/uniquindio/jokihairstyle/JokiHairstyleApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.jokihairstyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokiHairstyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokiHairstyleApplication.class, args);
+    }
+
+}
