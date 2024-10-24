451710171470ce63006b00d0d3a5a4ecb75c774e
dxniel-hit <dancorrea698@gmail.com>
2024-10-24 14:56:23 -0500

    More more few fixes

---
+++ src/main/java/co/edu/uniquindio/jokihairstyle/controllers/AdminController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.jokihairstyle.controllers;
+
+public class AdminController {
+}
--- src/main/java/co/edu/uniquindio/jokihairstyle/controllers/AuthController.java
+++ src/main/java/co/edu/uniquindio/jokihairstyle/controllers/AuthController.java
@@ -1,21 +1,43 @@
 package co.edu.uniquindio.jokihairstyle.controllers;
 
 
+import co.edu.uniquindio.jokihairstyle.dtos.AuthAdminDTO;
+import co.edu.uniquindio.jokihairstyle.dtos.LoginClientDTO;
+import co.edu.uniquindio.jokihairstyle.dtos.RegisterClientDTO;
 import co.edu.uniquindio.jokihairstyle.services.interfaces.AuthService;
 import co.edu.uniquindio.jokihairstyle.services.interfaces.ClientService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth")
 @RequiredArgsConstructor
-public class AuthenticationController {
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
 
     private final AuthService authService;
 
+    // Login for admin
+    @PostMapping("/admin/login")
+    public ResponseEntity<?> loginAdmin(@RequestBody @Valid AuthAdminDTO request) {
+        return authService.loginAdmin(request);
+    }
 
+    // Register client
+    @PostMapping("/client/register")
+    public ResponseEntity<?> registerClient(@RequestBody @Valid RegisterClientDTO request) {
+        return authService.registerClient(request);
+    }
+
+    // Login for client
+    @PostMapping("/client/login")
+    public ResponseEntity<?> loginClient(@RequestBody @Valid LoginClientDTO request) {
+        return authService.loginClient(request);
+    }
 }
+
